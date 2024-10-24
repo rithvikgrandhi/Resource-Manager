ee87884033e8b99b7de9080d93b09b5dad7d0c3b
Sathvik01 <svv.sathvik@gmail.com>
2024-10-24 11:24:48 +0530

    added context

---
+++ Backend/TalentSphere/Controllers/JobPostsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TalentSphere.Models;
+
+namespace TalentSphere.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobPostsController : ControllerBase
+    {
+        private readonly TalentsphereContext _context;
+
+        public JobPostsController(TalentsphereContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/JobPosts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<JobPost>>> GetJobPosts()
+        {
+          if (_context.JobPosts == null)
+          {
+              return NotFound();
+          }
+            return await _context.JobPosts.ToListAsync();
+        }
+
+        // GET: api/JobPosts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<JobPost>> GetJobPost(int id)
+        {
+          if (_context.JobPosts == null)
+          {
+              return NotFound();
+          }
+            var jobPost = await _context.JobPosts.FindAsync(id);
+
+            if (jobPost == null)
+            {
+                return NotFound();
+            }
+
+            return jobPost;
+        }
+
+        // PUT: api/JobPosts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutJobPost(int id, JobPost jobPost)
+        {
+            if (id != jobPost.JobPostId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(jobPost).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!JobPostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/JobPosts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<JobPost>> PostJobPost(JobPost jobPost)
+        {
+          if (_context.JobPosts == null)
+          {
+              return Problem("Entity set 'TalentsphereContext.JobPosts'  is null.");
+          }
+            _context.JobPosts.Add(jobPost);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetJobPost", new { id = jobPost.JobPostId }, jobPost);
+        }
+
+        // DELETE: api/JobPosts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteJobPost(int id)
+        {
+            if (_context.JobPosts == null)
+            {
+                return NotFound();
+            }
+            var jobPost = await _context.JobPosts.FindAsync(id);
+            if (jobPost == null)
+            {
+                return NotFound();
+            }
+
+            _context.JobPosts.Remove(jobPost);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool JobPostExists(int id)
+        {
+            return (_context.JobPosts?.Any(e => e.JobPostId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Backend/TalentSphere/Controllers/SkillsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TalentSphere.Models;
+
+namespace TalentSphere.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillsController : ControllerBase
+    {
+        private readonly TalentsphereContext _context;
+
+        public SkillsController(TalentsphereContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Skills
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Skill>>> GetSkills()
+        {
+          if (_context.Skills == null)
+          {
+              return NotFound();
+          }
+            return await _context.Skills.ToListAsync();
+        }
+
+        // GET: api/Skills/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Skill>> GetSkill(int id)
+        {
+          if (_context.Skills == null)
+          {
+              return NotFound();
+          }
+            var skill = await _context.Skills.FindAsync(id);
+
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            return skill;
+        }
+
+        // PUT: api/Skills/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSkill(int id, Skill skill)
+        {
+            if (id != skill.SkillId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(skill).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SkillExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Skills
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Skill>> PostSkill(Skill skill)
+        {
+          if (_context.Skills == null)
+          {
+              return Problem("Entity set 'TalentsphereContext.Skills'  is null.");
+          }
+            _context.Skills.Add(skill);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSkill", new { id = skill.SkillId }, skill);
+        }
+
+        // DELETE: api/Skills/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSkill(int id)
+        {
+            if (_context.Skills == null)
+            {
+                return NotFound();
+            }
+            var skill = await _context.Skills.FindAsync(id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            _context.Skills.Remove(skill);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SkillExists(int id)
+        {
+            return (_context.Skills?.Any(e => e.SkillId == id)).GetValueOrDefault();
+        }
+    }
+}
